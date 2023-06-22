b0404fd7b2340c280088aa866a78e99a1bb66a91
teich <roi.teichman@campus.technion.ac.il>
2023-06-22 15:33:52 +0300

    \r\n

--- webserver-files/request.c
+++ webserver-files/request.c
@@ -38,7 +38,7 @@ void requestError(int fd, char *cause, char *errnum, char *shortmsg, char *longm
    sprintf(buf, "%sStat-Thread-Id:: %ld\r\n", buf, id);
    sprintf(buf, "%sStat-Thread-Count:: %d\r\n", buf ,*total_counter);
    sprintf(buf, "%sStat-Thread-Static:: %d\r\n", buf, *static_counter);
-   sprintf(buf, "%sStat-Thread-Dynamic:: %d\r\n", buf, *dynamic_counter);
+   sprintf(buf, "%sStat-Thread-Dynamic:: %d\r\n\r\n", buf, *dynamic_counter);
    Rio_writen(fd, buf, strlen(buf));
    printf("%s", buf);
 
@@ -131,7 +131,7 @@ void requestServeDynamic(int fd, char *filename, char *cgiargs, struct timeval* 
     sprintf(buf, "%sStat-Thread-Id:: %ld\r\n", buf, id);
     sprintf(buf, "%sStat-Thread-Count:: %d\r\n", buf ,*total_counter);
     sprintf(buf, "%sStat-Thread-Static:: %d\r\n", buf, *static_counter);
-    sprintf(buf, "%sStat-Thread-Dynamic:: %d\r\n", buf, *dynamic_counter);
+    sprintf(buf, "%sStat-Thread-Dynamic:: %d\r\n\r\n", buf, *dynamic_counter);
 
     Rio_writen(fd, buf, strlen(buf));
 
@@ -174,7 +174,7 @@ void requestServeStatic(int fd, char *filename, int filesize, struct timeval* ar
    sprintf(buf, "%sStat-Thread-Id:: %ld\r\n", buf, id);
    sprintf(buf, "%sStat-Thread-Count:: %d\r\n", buf ,*total_counter);
    sprintf(buf, "%sStat-Thread-Static:: %d\r\n", buf, *static_counter);
-   sprintf(buf, "%sStat-Thread-Dynamic:: %d\r\n", buf, *dynamic_counter);
+   sprintf(buf, "%sStat-Thread-Dynamic:: %d\r\n\r\n", buf, *dynamic_counter);
 
    Rio_writen(fd, buf, strlen(buf));
 
