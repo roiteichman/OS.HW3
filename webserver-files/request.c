a429faefdbe1b26fe381e7c98129544118978b41
teich <roi.teichman@campus.technion.ac.il>
2023-06-22 15:56:41 +0300

    dynamic_request remove \r\n

--- webserver-files/request.c
+++ webserver-files/request.c
@@ -131,7 +131,7 @@ void requestServeDynamic(int fd, char *filename, char *cgiargs, struct timeval* 
     sprintf(buf, "%sStat-Thread-Id:: %ld\r\n", buf, id);
     sprintf(buf, "%sStat-Thread-Count:: %d\r\n", buf ,*total_counter);
     sprintf(buf, "%sStat-Thread-Static:: %d\r\n", buf, *static_counter);
-    sprintf(buf, "%sStat-Thread-Dynamic:: %d\r\n\r\n", buf, *dynamic_counter);
+    sprintf(buf, "%sStat-Thread-Dynamic:: %d\r\n", buf, *dynamic_counter);
 
     Rio_writen(fd, buf, strlen(buf));
 
